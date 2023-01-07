5647c6bcebfb9ba229bcc1d1f5ddbb29a007d6fc
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-07 17:11:06 -0300

    feat(arranger): calculate spaces for first lines

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,3 +1,6 @@
+def space_calc(longest, second):
+    return ' ' * (len(longest) - len(second)) + second
+
 def arithmetic_arranger(problems, display_answers=False):
     elements = []
     line1 = line2 = line3 = line4 = ''
@@ -10,6 +13,19 @@ def arithmetic_arranger(problems, display_answers=False):
     
         line1 += space * 2
         line2 += operator + space
+
+        if len(operand_a) >= len(operand_b):
+            longest = operand_a
+            second = operand_b
+
+            line1 += longest
+            line2 += space_calc(longest, second)
+        else:
+            longest = operand_b
+            second = operand_a
+
+            line1 += space_calc(longest, second)
+            line2 += longest
 
     arranged_problems = ''
 
