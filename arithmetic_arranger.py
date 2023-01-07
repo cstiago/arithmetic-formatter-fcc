810fd8827fc647b897ca88ccba31cdf134efb5a5
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-07 17:04:36 -0300

    feat(arranger): add start of first lines

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,8 +1,15 @@
 def arithmetic_arranger(problems, display_answers=False):
     elements = []
+    line1 = line2 = line3 = line4 = ''
+    space = ' '
 
-    for problem in problems:
+    for count, problem in enumerate(problems):
         elements.append(problem.split())
+
+        operand_a, operator, operand_b = elements[count]
+    
+        line1 += space * 2
+        line2 += operator + space
 
     arranged_problems = ''
 
