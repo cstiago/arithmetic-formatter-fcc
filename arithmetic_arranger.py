bb382da60c23e899c7f5917ebcc38090959342c9
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-07 16:51:20 -0300

    merge(develop): complete solution
    feat(arranger): add error handlers
    feat(arranger): add gap spaces and return
    feat(arranger): answer problems for fourth line
    feat(arranger): calculate dashes for third line
    feat(arranger): calculate spaces for first lines
    feat(arranger): add start of first lines
    feat(arranger): split problem elements

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,3 +1,74 @@
-def arithmetic_arranger(problems):
+def space_calc(longest, second):
+    return ' ' * (len(longest) - len(second)) + second
+
+def problem_length(longest):
+    return len(' ' * (2 + len(longest)))
+
+def dash_calc(longest):
+    return '-' * problem_length(longest)
+
+def arithmetic_arranger(problems, display_answers=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    elements = []
+    line1 = line2 = line3 = line4 = ''
+    space = ' '
+    gap = space * 4
+    skip = '\n'
+
+    for count, problem in enumerate(problems):
+        elements.append(problem.split())
+
+        operand_a, operator, operand_b = elements[count]
+
+        if operator != '+' and operator != '-':
+            return "Error: Operator must be '+' or '-'."
+
+        if not operand_a.isnumeric() or not operand_b.isnumeric():
+            return "Error: Numbers must only contain digits."
+
+        if len(operand_a) > 4 or len(operand_b) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        line1 += space * 2
+        line2 += operator + space
+
+        if len(operand_a) >= len(operand_b):
+            longest = operand_a
+            second = operand_b
+
+            line1 += longest
+            line2 += space_calc(longest, second)
+
+        else:
+            longest = operand_b
+            second = operand_a
+
+            line1 += space_calc(longest, second)
+            line2 += longest
+        
+        line3 += dash_calc(longest)
+
+        operand_a = int(operand_a)
+        operand_b = int(operand_b)
+
+        if operator == '+':
+            answer = operand_a + operand_b
+        elif operator == '-':
+            answer = operand_a - operand_b
+        
+        answer = str(answer)
+
+        line4 += space * (problem_length(longest) - len(answer)) + answer
+
+        if count != len(problems) - 1:
+            text = line1, line2, line3, line4
+            line1, line2, line3, line4 = [line + gap for line in text]
+
+    arranged_problems = line1 + skip + line2 + skip + line3
+
+    if display_answers:
+        arranged_problems += skip + line4
 
     return arranged_problems
